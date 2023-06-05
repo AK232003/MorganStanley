456dec16e453befedd081ca1f2d242e7e15a2d72
Chinthan23 <77500564+Chinthan23@users.noreply.github.com>
2023-06-05 20:02:06 +0530

    added dashboard styles

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,9 +4,13 @@ import { useNavigate } from "react-router-dom";
 const Dashboard= () =>{
   const navigate=useNavigate();
   return (
-    <div className="p-7">
-      <h1>DashBoard!</h1>
-      <Button className="rounded-2" onClick={()=> navigate('addChild')}> Add child</Button>
+    <div className="container m-4">
+      <div className="grid grid-cols-2 gap-2">
+      <div className="text-gray-500 justify-self-start rounded-4 bg-white px-3 py-1">
+         <div className="font-sans text-justify fw-bold text-4xl">Dashboard</div>
+      </div>
+        <button className=" col-2 justify-self-end rounded-pill bg-themecolor text-white" onClick={()=> navigate('addChild')}> Add child</button>
+      </div>
     </div>
   );
 }
