9583186f33729c1726fae504f6f308544ba686a2
Chinthan23 <77500564+Chinthan23@users.noreply.github.com>
2023-06-06 04:33:59 +0530

    made dashboard little responsive logout integrated successfully

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,11 +7,11 @@ const Dashboard= ({user}) =>{
   },[user])
   return (
     <div className="container m-4">
-      <div className="grid grid-cols-2 gap-2">
-      <div className="text-gray-500 justify-self-start rounded-4 bg-white px-3 py-1">
-         <div className="font-sans text-justify fw-bold text-4xl">Dashboard</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+      <div className="text-textcolor justify-self-start rounded-4 bg-color3 px-1 sm:px-3 py-1 drop-shadow-md ">
+         <div className="font-sans text-justify align-text-bottom fw-bold text-2xl sm:text-4xl">Dashboard</div>
       </div>
-        <button className=" col-2 justify-self-end rounded-pill bg-themecolor text-white" onClick={()=> navigate('addChild')}> Add child</button>
+        <button className="justify-self-start sm:justify-self-center p-2 rounded-pill bg-themecolor shadow-md drop-shadow-md text-white hover:shadow-themecolor/[0.5]"  onClick={()=> navigate('addChild')}> Add child</button>
       </div>
     </div>
   );
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,20 @@
-import {React,useState} from "react";
+import {React,useLayoutEffect,useState} from "react";
 import { FaBars, FaRegUserCircle, FaArrowAltCircleRight } from 'react-icons/fa';
 
 const SideBar=({setuser})=>{
 	const [openSide, toggle] = useState(true);
+  const [open,setOpen] =useState(false);
+  const handdleToggle=()=>{
+    if(!open) toggle(!openSide); 
+    open?setOpen(false):setOpen(open)
+  }
 	return (
-	<div className={`h-9/10 p-4 ${openSide ? "w-72" : "w-24"} bg-themecolor duration-300 rounded-3 m-2 relative shadow-xl opacity-90 hover:shadow-themecolor hover:opacity-100`}>
-          <FaBars className={`text-4xl cursor-pointer top-1 duration-500 ${openSide && "rotate-[180deg]"}`} onClick={() => toggle(!openSide)}></FaBars>
+    <>
+    <div className={`h-14 w-14 ${open && "hidden"} sm:hidden rounded-3 m-2 p-1 relative bg-sideBarColor1 opacity-90 top-3 left-5`}>
+          <FaBars className="h-12 w-12 cursor-pointer text-textcolor visible" onClick={() => setOpen(!open)}></FaBars>
+    </div>
+	<div className={`h-9/10 px-4 pb-4 pt-3 ${openSide ? "w-72" : "w-24"} ${!open && "hidden"} bg-sideBarColor1 duration-300 rounded-3 m-2 relative drop-shadow-2xl shadow-2xl opacity-90 hover:shadow-sideBarColor1 hover:opacity-100 sm:block`}>
+          <FaBars className={`h-12 w-12 cursor-pointer top-1 text-textcolor duration-500 visible ${openSide && "rotate-[180deg]"}`} onClick={() => handdleToggle()}></FaBars>
           {openSide &&
             <img alt="logo"
             src="logo_scroll.png"
@@ -13,20 +22,21 @@ const SideBar=({setuser})=>{
             width="100%"
             className="pt-4"></img>}
           <ul className="pt-2 ps-0">
-            <li className="text-xl text-gray-300 bg-themecolor rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-themecolor/[0.8]">1</li>
-            <li className="text-xl text-gray-300 bg-themecolor rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-themecolor/[0.8]">2</li>
-            <li className="text-xl text-gray-300 bg-themecolor rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-themecolor/[0.8]">3</li>
-            <li className="text-xl text-gray-300 bg-themecolor rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-themecolor/[0.8]">4</li>
-            <li className="text-xl text-gray-300 bg-themecolor rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-themecolor/[0.8]">
+            <li className="text-xl text-textcolor bg-color4 rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-color4/[0.8]">1</li>
+            <li className="text-xl text-textcolor bg-color4 rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-color4/[0.8]">2</li>
+            <li className="text-xl text-textcolor bg-color4 rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-color4/[0.8]">3</li>
+            <li className="text-xl text-textcolor bg-color4 rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-color4/[0.8]">4</li>
+            <li className="text-xl text-textcolor bg-color4 rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-color4/[0.8]">
               <span><FaRegUserCircle className="text-3xl text-textcolor block float-left"></FaRegUserCircle></span>
               <span className={`text-base font-medium flex-1 ${!openSide && "hidden"}`}>Profile</span>
             </li>
-            <li className="text-xl text-gray-300 bg-themecolor rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-themecolor/[0.8]">
-              <span><FaArrowAltCircleRight className="text-3xl text-textcolor block float-left cursor-pointer" onClick={()=>setuser(null)} ></FaArrowAltCircleRight></span>
+            <li className="text-xl text-textcolor bg-color4 rounded-md p-2 flex items-center gap-x-4 mt-2 hover:bg-color4/[0.8] cursor-pointer" onClick={()=>setuser(null)} >
+              <span><FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight></span>
               <span className={`text-base font-medium flex-1 ${!openSide && "hidden"}`}>Logout</span>
             </li>
           </ul>
         </div>
+        </>
 	)
 };
 
