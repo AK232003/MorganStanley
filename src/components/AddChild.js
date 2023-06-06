41b66ed586b0a5dd4f8e310b7f03fdf7661fc5a6
Chinthan23 <77500564+Chinthan23@users.noreply.github.com>
2023-06-06 18:09:40 +0530

    small change

--- src/components/AddChild.js
+++ src/components/AddChild.js
@@ -1,7 +1,10 @@
 import { React, useState, useEffect } from "react";
 import { Accordion, AccordionBody, AccordionHeader, AccordionItem, Form, FormGroup, FormText, FormFeedback, Label, Input, Col, Button } from 'reactstrap';
 import { useNavigate,Link } from "react-router-dom";
+import { database, db } from "../firebase";
+
 const AddChild = ({user}) => {
+
 	const [open, setOpen] = useState('1');
 	const toggle = (id) => {
 		if (open === id) {
@@ -10,20 +13,64 @@ const AddChild = ({user}) => {
 			setOpen(id);
 		}
 	};
-	const handleSubmitInformation = (event) => {
-		event.preventDefault();
-		console.log(event);
+	const handleSubmitInformation = (element) => {
+		element.preventDefault();
+		console.log(element.target[1].value);
+		// const id = element["Case Number"].split("/").join("");
+		const id = element.target[11].value.split("/").join("");
+        db.collection("children")
+          .doc(id)
+          .set(
+			{
+				"Name": element.target[1].value,
+				"Gender": element.target[2].value,
+				"Date Of Birth": element.target[3].value,
+				"Age": element.target[4].value,
+				"Child Category":element.target[5].value,
+				"Image": element.target[6].value,
+				"State": element.target[7].value,
+				"District": element.target[8].value,
+				"Home": element.target[9].value,
+				"Case Number": element.target[11].value,
+				"Reason For Admission": element.target[12].value,
+				"Reason For Flagging": element.target[13].value,
+				"Last Visit Since": element.target[14].value,
+				"Last Call Since": element.target[15].value,
+				"Guardian": element.target[16].value,
+				"Sibling": element.target[17].value,
+				"Total Shelter Home Stay": element.target[18].value,
+				"CWC Last Review": element.target[19].value,
+				"Last CWC Order": element.target[20].value,
+				"Case History": element.target[21].value,
+				"Documents Completed": element.target[23].value,
+				"Documents Pending": element.target[24].value,
+				"News Paper Publications Pending": element.target[25].value,
+				"Police Report Pending": element.target[26].value,
+				"Surrender Pending": element.target[27].value,
+				"Status": element.target[28].value,
+			}
+		  )
+          .then(() => {
+            console.log("Document successfully written with ID: ", id);
+            // Create an entry in the Realtime Database for the child profile
+            database
+              .ref("childProfile/" + id)
+              .set(db.collection("children").doc(id).id);
+          })
+          .catch((error) => {
+            console.error("Error writing document: ", error);
+          });
 	}
 	const navigate=useNavigate();
   useEffect(()=>{
     if(user===null) navigate("/");
   },[user])
 	return (
-		<div className="container mt-4">
-			<Form onSubmit={(event) => handleSubmitInformation(event)}>
-			<Accordion open={open} toggle={toggle}>
+		<div className="container mt-4" >
+			<Accordion className="overflow-y-scroll overflow-x-hidden h-full" open={open} toggle={toggle}>
+			<Form  onSubmit={(event) => handleSubmitInformation(event)}>
 				<AccordionItem>
-					<AccordionHeader targetId="1">Information</AccordionHeader>
+					<AccordionHeader targetId="1">Section-1</AccordionHeader>
 					<AccordionBody accordionId="1">
 							<FormGroup row>
 								<Label for="fullname" sm={2}> Full Name </Label>
@@ -58,95 +105,124 @@ const AddChild = ({user}) => {
 								</Col>
 							</FormGroup>
 							<FormGroup row>
-								<Label for="birthplace" sm={2}> Place of Birth </Label>
+								<Label for="image" sm={2}> Image </Label>
 								<Col sm={10}>
-									<Input id="birthplace" name="birthplace" placeholder="Birth Place" type="text" /></Col>
+									<Input id="image" name="image" placeholder="Image" type="file" accept=".jpeg, .jpg, .png" /></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="state" sm={2}> State </Label>
+								<Col sm={10}>
+									<Input id="state" name="state" placeholder="State" type="text" /></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="disctrict" sm={2}> District </Label>
+								<Col sm={10}>
+									<Input id="disctrict" name="disctrict" placeholder="Disctrict" type="text" /></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="home" sm={2}> Home </Label>
+								<Col sm={10}>
+									<Input id="home" name="home" placeholder="Home" type="text" /></Col>
+							</FormGroup>
+							</AccordionBody>
+							<AccordionHeader targetId="2">Section-2</AccordionHeader>
+							<AccordionBody accordionId="2">
+							<FormGroup row>
+								<Label for="caseno" sm={2}> Case Number </Label>
+								<Col sm={10}>
+									<Input id="caseno" name="caseno" placeholder="Case Number" type="text" /></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="rfa" sm={2}> Reason For Admission </Label>
+								<Col sm={10}>
+									<Input id="rfa" name="rfa" placeholder="Reason For Admission" type="textarea" /></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="rff" sm={2}> Reason For Flagging </Label>
+								<Col sm={10}>
+									<Input id="rff" name="rff" placeholder="Reason For Flagging" type="text" /></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="lvs" sm={2}> Last Visit Since<span className="text-red-500">*</span> </Label>
+								<Col sm={10}>
+									<Input id="lvs" name="lvs" placeholder="Last Visit Since" type="text" /></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="lcs" sm={2}> Last Call Since </Label>
+								<Col sm={10}>
+									<Input id="lcs" name="lcs" placeholder="Last Call Since" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="guardian" sm={2}> Guardian </Label>
+								<Col sm={10}>
+									<Input id="guardian" name="guardian" placeholder="Guardian" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="sibling" sm={2}>Sibling Details</Label>
+								<Col sm={10}>
+									<Input id="sibling" name="sibling" placeholder="Sibling Details" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="tshs" sm={2}>Total Shelter Home Stay</Label>
+								<Col sm={10}>
+									<Input id="tshs" name="tshs" placeholder="Total Shelter Home Stay" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="cwclr" sm={2}>CWC Last Review</Label>
+								<Col sm={10}>
+									<Input id="cwclr" name="cwclr" placeholder="CWC last review" type="date"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="cwclo" sm={2}>Last CWC Order</Label>
+								<Col sm={10}>
+									<Input id="cwclo" name="cwclo" placeholder="CWC last order" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="casehistory" sm={2}>Case History</Label>
+								<Col sm={10}>
+									<Input id="casehistory" name="casehistory" placeholder="Case History" type="textarea"/></Col>
+							</FormGroup>
+							</AccordionBody>
+							<AccordionHeader targetId="3">Section-3</AccordionHeader>
+							<AccordionBody accordionId="3">
+							<FormGroup row>
+								<Label for="docscomp" sm={2}>Documents Completed</Label>
+								<Col sm={10}>
+									<Input id="docscomp" name="docscomp" placeholder="Documents Completed" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="docspend" sm={2}>Documents Pending</Label>
+								<Col sm={10}>
+									<Input id="docspend" name="docspend" placeholder="Documents Pending" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="newspend" sm={2}>News Paper Publications Pending</Label>
+								<Col sm={10}>
+									<Input id="newspend" name="newspend" placeholder="News Paper Publications Pending" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="policepend" sm={2}>Police Report Pending</Label>
+								<Col sm={10}>
+									<Input id="policepend" name="policepend" placeholder="Police Report Pending" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="surrenderpend" sm={2}>Surrender Pending</Label>
+								<Col sm={10}>
+									<Input id="surrenderpend" name="surrenderpend" placeholder="Surrender Pending" type="text"/></Col>
+							</FormGroup>
+							<FormGroup row>
+								<Label for="status" sm={2}>Status</Label>
+								<Col sm={10}>
+								<Input id="status" name="status" type="select" >
+										<option>Assigned</option>
+										<option>Not Assigned</option>
+										<option>Completed </option>
+									</Input>
+								</Col>
 							</FormGroup>
 					</AccordionBody>
 				</AccordionItem>
-				<AccordionItem>
-					<AccordionHeader targetId="2">Family Background</AccordionHeader>
-					<AccordionBody accordionId="2">
-					<FormGroup row>
-						<Label for="parentname" sm={2}> Name of Parent </Label>
-						<Col sm={10}>
-							<Input id="parentname" name="parentname" placeholder="Parent Name" type="text" /></Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label for="occupation" sm={2}> Occupation </Label>
-						<Col sm={10}>
-							<Input id="occupation" name="occupation" placeholder="Occupation" type="text" /></Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label for="income" sm={2}> Income </Label>
-						<Col sm={10}>
-							<Input id="income" name="income" placeholder="Income" type="number" /></Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label for="address" sm={2}> Address </Label>
-						<Col sm={10}>
-							<Input id="address" name="address" placeholder="Address" type="textarea" /></Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label for="infor" sm={2}> Information of Family Numbers </Label>
-						<Col sm={10}>
-							<Input id="infor" name="infor" placeholder="Family information" type="textarea" /></Col>
-					</FormGroup>
-					
-					<FormGroup row>
-						<Label for="reason" sm={2}> Reason for addmission </Label>
-						<Col sm={10}>
-							<Input id="reason" name="reason" placeholder="Reason" type="textarea" /></Col>
-					</FormGroup>
-					</AccordionBody>
-				</AccordionItem>
-				<AccordionItem>
-					<AccordionHeader targetId="3">Medical History</AccordionHeader>
-					<AccordionBody accordionId="3">
-					<FormGroup row>
-						<Label for="medicalinfo" sm={2}> Name of Parent </Label>
-						<Col sm={10}>
-							<Input rows={10} id="medicalinfo" name="medicalinfo" placeholder={`Medical History \n Existing Medical Conditions \n Known Allergies \n Vaccination History \n Previous Medical treatments/surgeriess \n Current Medication`} type="textarea" /></Col>
-					</FormGroup>
-					</AccordionBody>
-				</AccordionItem>
-				<AccordionItem>
-					<AccordionHeader targetId="4">Education and Skills</AccordionHeader>
-					<AccordionBody accordionId="4">
-					<FormGroup row>
-						<Label for="education" sm={2}> Education and Skills </Label>
-						<Col sm={10}>
-							<Input rows={10} id="education" name="education" placeholder={`Educational background \n School Enrollment Details\n Special Skills/Talents`} type="textarea" /></Col>
-					</FormGroup>
-					</AccordionBody>
-				</AccordionItem>
-				<AccordionItem>
-					<AccordionHeader targetId="5">Documents</AccordionHeader>
-					<AccordionBody accordionId="5">
-					<FormGroup row>
-						<Label for="birthcertificate" sm={2} > Birth Certificate </Label>
-						<Col sm={10}> <Input id="birthcertificate" name="birthcertificate" type="file"/>
-						</Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label for="oprhancertificate" sm={2} > Orphan Certificate </Label>
-						<Col sm={10}> <Input id="oprhancertificate" name="oprhancertificate" type="file"/>
-						</Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label for="socialinvesreport" sm={2} > Social Investigation Report </Label>
-						<Col sm={10}> <Input id="socialinvesreport" name="socialinvesreport" type="file"/>
-						</Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label for="childimage" sm={2} > Child Image </Label>
-						<Col sm={10}> <Input id="childimage" name="childimage" type="file" accept=".jpeg.jpg.png"/>
-						</Col>
-					</FormGroup>
-					</AccordionBody>
-				</AccordionItem>
-			</Accordion>
-			<FormGroup row>
+				<FormGroup row>
 				<div className="col-2 m-2">
 					<Button type="submit" color="primary">
 						Submit
@@ -159,8 +235,10 @@ const AddChild = ({user}) => {
 					</Button>
 					</Link>
 				</div>
-			</FormGroup>
+			</FormGroup>			
 		</Form>
+		
+		</Accordion>
 		</div>
 	);
 }
