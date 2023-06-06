0b852dd88f2c15e4861b0f723f51ecbcfafdd0a2
KMishra23 <kaushik.ultimate96@gmail.com>
2023-06-06 18:42:15 +0530

    fixed login issue for case manager

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ import Dashboard from "./components/Dashboard";
 import Dashboard1 from "./components/DashBoard1";
 import SideBar from "./components/SideBar";
 import AddChild from "./components/AddChild";
-import ListTest from "./components/ChildrenList";
-
+import AddchildXL from "./components/addChildxl";
 function App() {
   const [user, setUser] = useState(null);
   return (
@@ -24,10 +23,9 @@ function App() {
               <Route index element={<Login setuser={(value)=>setUser(value)}/>}></Route>
               <Route exact path="groundWorker" element={<Dashboard user={user}/>}/>
               <Route path="groundWorker/addChild" element={<AddChild user={user}/>}/>
-              <Route path="groundWorker/addExcel" element={<Dashboard1 />}/>
+              <Route path="groundWorker/addExcel" element={<AddchildXL />}/>
               <Route path="signup" element={<Signup />} />
               <Route path="/caseManager" element={<Dashboard1 />} />
-              <Route path="groundWorker/list" element={< ListTest user={user}/>} />
             </Routes>
           </AuthProvider>
         </Router>
@@ -37,3 +35,4 @@ function App() {
 }
 
 export default App;
+
