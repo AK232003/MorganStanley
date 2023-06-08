2029e8f69862cbedc8acd3e03dd453543c526585
Rakshit <rakshitbang05@gmail.com>
2023-06-08 14:51:22 +0530

    SideNav and Case Profile Page UI update

--- src/components/groundWorker/Reports.js
+++ src/components/groundWorker/Reports.js
@@ -1,6 +1,17 @@
 import React, {useEffect, useState} from "react";
 import {  useLocation } from "react-router-dom";
-import { Form,FormGroup, Col, Label,Input,Button } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Form,
+  FormGroup,
+  Col,
+  Label,
+  Input,
+  Button,
+} from "reactstrap";
 import { getDownloadURL, ref as storageRef, uploadBytes, } from "firebase/storage";
 import { database, db, storage } from "../../firebase";
 const Report=() => {
@@ -8,6 +19,9 @@ const Report=() => {
 	const [type,setType]=useState(location.pathname.split("/")[5]);
 	const [step,setStep]=useState("");
 	const [msg, setMsg] = useState("");
+	const [newsReportText, setNewsReportText] = useState("");
+	const [comments, setComments] = useState("");
+  	const [submitted, setSubmitted] = useState(false);
 	const [status,setStatus]=useState("Loading");
 	const id=location.pathname.split("/")[3];
 	const [imageUpload, setImageUpload] = useState(null);
@@ -29,21 +43,23 @@ const Report=() => {
 	},[location])
 	const handleSubmitInformation = ((e) => {
 		e.preventDefault();
-		const imageRef = storageRef(storage, `documents/${id}/NewsPublicationReport`);
-		uploadBytes(imageRef, imageUpload)
-			.then((snapshot) => {
-			  getDownloadURL(snapshot.ref).then((url) => {
-				db.collection("cases").doc(id).update({
-				  "Photo Publication Report": url,
-				  "Photo Publication Text": e.target[0].value
-				})
-			  })
-			})
+		// const imageRef = storageRef(storage, `documents/${id}/NewsPublicationReport`);
+		// uploadBytes(imageRef, imageUpload)
+		// 	.then((snapshot) => {
+		// 	  getDownloadURL(snapshot.ref).then((url) => {
+		// 		db.collection("cases").doc(id).update({
+		// 		  "Photo Publication Report": url,
+		// 		  "Photo Publication Text": e.target[0].value
+		// 		})
+		// 	  })
+		// 	})
 
-			database.ref("childProfile/"+`${id}`).update({[step] :"In Progress"})
-			database.ref("childProfile/"+`${id}`).on('value',(snapshot)=>{
-				setStatus(snapshot.val()[step]);
-			})
+		// 	database.ref("childProfile/"+`${id}`).update({[step] :"In Progress"})
+		// 	database.ref("childProfile/"+`${id}`).on('value',(snapshot)=>{
+		// 		setStatus(snapshot.val()[step]);
+		// 	})
+
+		setSubmitted(true);
 	  
 	  })
 
@@ -54,46 +70,123 @@ const Report=() => {
 			// 	setStatus(snapshot.val()[step]);
 			});
 	//   })
+
+	const SendComment = (e) => {
+        e.preventDefault();
+        // Handle submit logic here
+        setSubmitted(true);
+      };
 	return (
-			<div className="h-95 w-95 sm:h-9/10 sm:w-2/5 bg-sideBarColor1  rounded-1 ms-3 m-3 sm:m-2 sm:relative drop-shadow-xl shadow-xl opacity-90 hover:shadow-sideBarColor1 hover:opacity-100 sm:block align-items-center justify-content-center overflow-y-scroll"> 
-			<div className="row mt-3 m-2 w-95"> 
-				<div className="col p-0 font-bold text-3xl"> {mapOfTypes.get(type)[0]}</div>
-				<div className="col-auto p-0"></div>
-				<div className="col-auto p-0 justify-self-end "> <div className="w-auto rounded-pill font-medium bg-color2 justify-self-center p-2"> {status}</div> </div>
-			</div>
-			<div>
-				Manager Message: <Label>{msg}</Label>
-			</div>
-			<div className="row m-2 mt-3 w-95">	
-			<Form onSubmit={(e)=>handleSubmitInformation(e)}>
-			<FormGroup row>
-								<Label for="newsreporttext" sm={2}>News Report Text </Label>
-								<Col sm={10}>
-									<Input id="newsreporttext" name="newsreporttext" placeholder="News Report Text" type="textarea" rows={3}/></Col>
-							</FormGroup>
-							
-							<FormGroup row>
-								<Label for="image" sm={2}> Image </Label>
-								<Col sm={10}>
-									<Input id="image" name="image" placeholder="Image" type="file"  onChange={(e) => {setImageUpload(e.target.files[0]);}}/></Col>
-							</FormGroup>
-							<Button className="!bg-color3 !border-none !text-textcolor" type="submit" >
-						Submit
-					</Button>
-			</Form>
-			<Form onSubmit={(e)=>handleSubmitInformation1(e)}>
-			<FormGroup row>
-				<Label for="comments" sm={2}>Comments </Label>
-				<Col sm={10}>
-					<Input id="comments" name="comments" placeholder="Comments" type="textarea" rows={3}/></Col>
-			</FormGroup>
-			<Button className="!bg-color3 !border-none !text-textcolor" type="submit" >
-				Submit
-			</Button>
-			</Form>
-			</div>
-		</div>
-	)
+    <div className="h-95 w-95 sm:h-9/10 sm:w-2/5 bg-sideBarColor1  rounded-1 ms-3 m-3 sm:m-2 sm:relative drop-shadow-xl shadow-xl opacity-90 hover:shadow-sideBarColor1 hover:opacity-100 sm:block align-items-center justify-content-center overflow-y-scroll">
+      <div className="row mt-3 m-2 w-95">
+        <div className="col p-0 font-bold text-3xl">
+          {" "}
+          {mapOfTypes.get(type)[0]}
+        </div>
+        <div className="col-auto p-0"></div>
+        <div className="col-auto p-0 justify-self-end ">
+          {" "}
+          <div className="w-auto rounded-pill font-medium bg-color2 justify-self-center p-2">
+            {" "}
+            {status}
+          </div>{" "}
+        </div>
+      </div>
+
+      <div className="row m-2 mt-3 w-95">
+        <Card className="my-4">
+          <CardBody>
+            <CardTitle tag="h5">Event Report:</CardTitle>
+
+            <Form onSubmit={handleSubmitInformation}>
+              <FormGroup row>
+                <Label for="newsreporttext" sm={2}>
+                  Report:
+                </Label>
+                <div className="col-sm-10">
+                  <Input
+                    type="textarea"
+                    name="newsreporttext"
+                    id="newsreporttext"
+                    value={newsReportText}
+                    onChange={(e) => setNewsReportText(e.target.value)}
+                    placeholder="Enter news report text"
+                    rows={3}
+                  />
+                </div>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label for="image" sm={2}>
+                  Files:
+                </Label>
+                <div className="col-sm-10">
+                  <Input
+                    type="file"
+                    name="image"
+                    id="image"
+                    onChange={(e) => {
+                      setImageUpload(e.target.files[0]);
+                    }}
+                  />
+                </div>
+              </FormGroup>
+
+              <Button
+                className="bg-color3 border-none text-textcolor"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+
+            {submitted && (
+              <div className="mt-4">
+                <h6>Your event report has been submitted.</h6>
+                {/* Additional logic or message for submitted form */}
+              </div>
+            )}
+          </CardBody>
+        </Card>
+
+        <Card className="my-4">
+          <CardBody>
+            <CardTitle tag="h5">Manager Message:</CardTitle>
+            <CardSubtitle className="mb-3">{msg}</CardSubtitle>
+
+            <Form onSubmit={SendComment}>
+              <FormGroup>
+                <Label for="comments">Comments:</Label>
+                <Input
+                  type="textarea"
+                  name="comments"
+                  id="comments"
+                  value={comments}
+                  onChange={(e) => setComments(e.target.value)}
+                  placeholder="Enter your comments"
+                  rows={3}
+                />
+              </FormGroup>
+
+              <Button
+                className="bg-color3 border-none text-textcolor"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+
+            {submitted && (
+              <div className="mt-4">
+                <h6>Your comment has been submitted.</h6>
+                {/* Additional logic or message for submitted form */}
+              </div>
+            )}
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default Report;
--- src/components/groundWorker/groundWorkerSidebar.js
+++ src/components/groundWorker/groundWorkerSidebar.js
@@ -10,7 +10,8 @@ const GroundWorkerSidebar=({user,setuser,child})=>{
 	const navigate=useNavigate();
 	const location=useLocation().pathname;
   const sideBarProperty = "";
-  const sideBarIconProperty = "text-xl text-textcolor w-full bg-color3 rounded-1 p-2 flex-column justify-start  gap-x-4 mt-2 cursor-pointer";
+  const sideBarIconProperty =
+    "text-xl text-textcolor w-full bg-color3 rounded-1 p-2 flex-column justify-start gap-x-4 mt-2 cursor-pointer transition duration-300 hover:bg-gray-400 position-relative overflow-hidden";
 	const toggleAccordion=(id)=>{
 		if(active===id) setActive(0);
 		else setActive(id);
@@ -32,121 +33,264 @@ const GroundWorkerSidebar=({user,setuser,child})=>{
 	},[location])
 	return (
     <>
-    <div className={`h-14 w-14 ${!open && "row"} sm:!hidden rounded-1 m-2 p-1 relative bg-sideBarColor1 opacity-90 top-4 left-5 z-0 ${open&& "opacity-0"} overflow-hidden`}>
-          <FaBars className="h-full w-full p-0 cursor-pointer text-textcolor visible" onClick={() =>{ setOpen(!open); toggle(true)}}></FaBars>
-    </div>
-	<div className={`h-95 sm:h-9/10 px-4 pb-4 pt-3 ${openSide ? "w-72" : "w-24"} ${!open && "hidden"} ${open && "w-95"} bg-sideBarColor1 duration-300 rounded-1 m-2 absolute top-1 sm:relative drop-shadow-2xl shadow-2xl opacity-90 hover:shadow-sideBarColor1 hover:opacity-100 sm:block z-10`}>
-          <FaBars className={`h-12 w-12 cursor-pointer top-1 text-textcolor duration-500 visible ${openSide && "rotate-[180deg]"}`} onClick={() => handdleToggle()}></FaBars>
-          {openSide &&
-            <img alt="logo"
+      <div
+        className={`h-14 w-14 ${
+          !open && "row"
+        } sm:!hidden rounded-1 m-2 p-1 relative bg-sideBarColor1 opacity-90 top-4 left-5 z-0 ${
+          open && "opacity-0"
+        } overflow-hidden`}
+      >
+        <FaBars
+          className="h-full w-full p-0 cursor-pointer text-textcolor visible"
+          onClick={() => {
+            setOpen(!open);
+            toggle(true);
+          }}
+        ></FaBars>
+      </div>
+      <div
+        className={`h-95 sm:h-9/10 px-4 pb-4 pt-3 ${
+          openSide ? "w-72" : "w-24"
+        } ${!open && "hidden"} ${
+          open && "w-95"
+        } bg-sideBarColor1 duration-300 rounded-1 m-2 absolute top-1 sm:relative drop-shadow-2xl shadow-2xl opacity-90 hover:shadow-sideBarColor1 hover:opacity-100 sm:block z-10`}
+      >
+        <FaBars
+          className={`h-12 w-12 cursor-pointer top-1 text-textcolor duration-500 visible ${
+            openSide && "rotate-[180deg]"
+          }`}
+          onClick={() => handdleToggle()}
+        ></FaBars>
+        {openSide && (
+          <img
+            alt="logo"
             src={logo}
             height="40"
             width="100%"
-            className="pt-4"></img>}
-            {user === "groundWorker" && 
-            <ul className="pt-2 ps-0">
-							<li> 
-								<div className={sideBarIconProperty} onClick={()=>toggleAccordion(1)} >
-                <span><FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight></span>
-                <span className={`text-base font-medium ms-2 ${!openSide && "hidden"}`}>Step-1</span> 
-								{active===1 && openSide && <ul className="pt-1 text-base font-medium">
-									<li> 
-								<Link to="step1/newsPaperReport" state={{child: child}} className="no-underline">
-										<button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor"> 
-										News paper Report
-									</button>
-									</Link>
-										</li>
-									<li> 
-										<Link to="step1/TVReport" state={{child: child}} className="no-underline">
-										<button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor"> 
-										TV Report
-									</button>
-										</Link>
-										</li>
-									<li> 
-										<Link to="step1/fileMisingReport" state={{child: child}} className="no-underline">
-										<button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor"> 
-										File Missing Report
-									</button>
-										</Link>
-										</li>
-									<li> 
-										<Link to="step1/medicalReport" state={{child: child}} className="no-underline">
-										<button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor"> 
-										Medical Report
-									</button>
-										</Link>
-										</li>
-									<li> 
-										<Link to="step1/siReport" state={{child: child}} className="no-underline">
-										<button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor"> 
-										SI Report
-									</button>
-										</Link>
-										</li>
-									</ul>}
-								</div>
-							</li>
-							<li> 
-								<div className={sideBarIconProperty} onClick={()=>toggleAccordion(2)} >
-								<span><FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight></span>
-                <span className={`text-base font-medium ms-2 ${!openSide && "hidden"} `}>Step-2</span> 
-								{active===2 && openSide && <ul className="pt-1 text-base font-medium">
-									<li> 
-								<Link to="step2" state={{child: child}} className="no-underline" disabled>
-										<button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 "> 
-										News paper Report
-									</button>
-									</Link>
-										</li>
-									<li> <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 "> 
-										TV Report
-									</button>
-										</li>
-									<li> <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 "> 
-										File Missing Report
-									</button>
-										</li>
-									<li> <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 "> 
-										Medical Report
-									</button>
-										</li>
-									<li> <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 "> 
-										SI Report
-									</button>
-										</li>
-									</ul>}
-								</div>
-							</li>
-							<li> 
-								<div className={sideBarIconProperty} onClick={()=>toggleAccordion(3)} disabled>
-                <span><FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight></span>
-                <span className={`text-base font-medium ms-2 ${!openSide && "hidden"}`}>Step-3</span> 
-								{active===3 && openSide && <ul className="pt-1 text-base font-medium">
-									<li> 
-								<Link to="step3/newspaper" state={{child: child}} className="no-underline" disabled>
-										<button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 "> 
-										News paper Report
-									</button>
-									</Link>
-										</li>
-									<li> <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 "> 
-										TV Report
-									</button>
-										</li>
-									</ul>}
-								</div>
-							</li>
-							<li> <button className={sideBarIconProperty} onClick={()=>handleLogout()} >
-                <span><FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight></span>
-                <span className={`text-base font-medium ms-2 ${!openSide && "hidden"}`}>Logout</span> </button>
-							</li>
-            </ul>
-            }
-        </div>
-        </>
-	)
+            className="pt-4"
+          ></img>
+        )}
+        {user === "groundWorker" && (
+          <ul className="pt-2 ps-0">
+            {/* Step-1 Button */}
+            <li>
+              <div
+                className={sideBarIconProperty}
+                onClick={() => toggleAccordion(1)}
+              >
+                <span>
+                  <FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight>
+                </span>
+                <span
+                  className={`text-base font-medium ms-2 ${
+                    !openSide && "hidden"
+                  }`}
+                >
+                  Step-1: Document Completition
+                </span>
+                {active === 1 && openSide && (
+                  <ul className="pt-1 text-base font-medium">
+                    <li>
+                      <Link
+                        to="step1/newsPaperReport"
+                        state={{ child: child }}
+                        className="no-underline"
+                      >
+                        <button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor">
+                          News paper Report
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="step1/TVReport"
+                        state={{ child: child }}
+                        className="no-underline"
+                      >
+                        <button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor">
+                          TV Report
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="step1/fileMisingReport"
+                        state={{ child: child }}
+                        className="no-underline"
+                      >
+                        <button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor">
+                          File Missing Report
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="step1/medicalReport"
+                        state={{ child: child }}
+                        className="no-underline"
+                      >
+                        <button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor">
+                          Medical Report
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="step1/siReport"
+                        state={{ child: child }}
+                        className="no-underline"
+                      >
+                        <button className="flex-column justify-items-start bg-color2/[0.6] m-1  p-1 w-95 rounded-2 text-textcolor">
+                          SI Report
+                        </button>
+                      </Link>
+                    </li>
+                  </ul>
+                )}
+              </div>
+            </li>
+
+            {/* <li>
+              <div
+                className={sideBarIconProperty}
+                onClick={() => toggleAccordion(2)}
+              >
+                <span>
+                  <FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight>
+                </span>
+                <span
+                  className={`text-base font-medium ms-2 ${
+                    !openSide && "hidden"
+                  } `}
+                >
+                  Step-2
+                </span>
+                {active === 2 && openSide && (
+                  <ul className="pt-1 text-base font-medium">
+                    <li>
+                      <Link
+                        to="step2"
+                        state={{ child: child }}
+                        className="no-underline"
+                        disabled
+                      >
+                        <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 ">
+                          News paper Report
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
+                      {" "}
+                      <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 ">
+                        TV Report
+                      </button>
+                    </li>
+                    <li>
+                      {" "}
+                      <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 ">
+                        File Missing Report
+                      </button>
+                    </li>
+                    <li>
+                      {" "}
+                      <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 ">
+                        Medical Report
+                      </button>
+                    </li>
+                    <li>
+                      {" "}
+                      <button className="flex-column justify-items-start bg-color2/[0.6] m-1 p-1 w-full rounded-2 ">
+                        SI Report
+                      </button>
+                    </li>
+                  </ul>
+                )}
+              </div>
+            </li> */}
+            {/* Step-2 Button */}
+            <li>
+              {" "}
+              <button
+                className={sideBarIconProperty}
+                onClick={() => toggleAccordion(2)}
+              >
+                <span>
+                  <FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight>
+                </span>
+                <span
+                  className={`text-base font-medium ms-2  ${
+                    !openSide && "hidden"
+                  }`}
+                >
+                  Step-2: Get NOC Certificate
+                </span>{" "}
+              </button>
+            </li>
+
+            {/* Step-3 Button */}
+            <li>
+              {" "}
+              <button
+                className={sideBarIconProperty}
+                onClick={() => toggleAccordion(3)}
+              >
+                <span>
+                  <FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight>
+                </span>
+                <span
+                  className={`text-base font-medium ms-2  ${
+                    !openSide && "hidden"
+                  }`}
+                >
+                  Step-3: Get LFA Certificate
+                </span>{" "}
+              </button>
+            </li>
+
+            {/* Step-4 Button */}
+            <li>
+              {" "}
+              <button
+                className={sideBarIconProperty}
+                onClick={() => toggleAccordion(4)}
+              >
+                <span>
+                  <FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight>
+                </span>
+                <span
+                  className={`text-base font-medium ms-2  ${
+                    !openSide && "hidden"
+                  }`}
+                >
+                  Step-4: Upload Child to CARINGS
+                </span>{" "}
+              </button>
+            </li>
+
+            {/* Logout Button */}
+            <li>
+              {" "}
+              <button
+                className={sideBarIconProperty}
+                onClick={() => handleLogout()}
+              >
+                <span>
+                  <FaArrowAltCircleRight className="text-3xl text-textcolor block float-left"></FaArrowAltCircleRight>
+                </span>
+                <span
+                  className={`text-base font-medium ms-2 ${
+                    !openSide && "hidden"
+                  }`}
+                >
+                  Logout
+                </span>{" "}
+              </button>
+            </li>
+          </ul>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default GroundWorkerSidebar;
