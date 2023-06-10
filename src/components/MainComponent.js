f3dfb006b663ba6a99404f12f8d5b0bad2f3f4e9
Chinthan23 <77500564+Chinthan23@users.noreply.github.com>
2023-06-11 01:07:32 +0530

    logout icon fixed

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -26,13 +26,14 @@ const Main=({user,setUser}) =>{
   }, []);
 	return(
 		<>
-		<div className={`flex flex-row w-full`}>
+		<div className={`flex flex-row w-full h-screen`}>
 			<div className="flex pd-0">
 				<SideBar user={user} setuser={(value)=>setUser(value)} open={open} setOpen={(value)=>setOpen(value)} handdleToggle={()=>handleToggle()} openSide={openSide} toggle={toggle}/>
 			</div>
 			<div className={`flex flex-col w-full ${open && "blur-sm"}`}>
 				<NavBar user={user} open={open} setOpen={(value)=>setOpen(value)} toggle={toggle}/>
-				<div className={`container mx-auto mt-1 lg:ms-2 bg-color2 overflow-y-scroll}`}>
+				{console.log(openSide, width)}
+				<div className={`container mx-auto mt-1 lg:ms-2 bg-color2 overflow-y-scroll max-h-screen}`}>
 				<Outlet/>
 				</div>
 			</div>
